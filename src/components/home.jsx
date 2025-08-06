d1cb9165802bb0bb0534e5cf3cb6cf7df0e069f4
Catherine Nixon <catherinenixon1b30@gmail.com>
2025-08-06 19:14:56 +0530

    edited home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography, Button, Paper, List, ListItem, ListItemText } from '@mui/material';
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <Container maxWidth="sm" sx={{ mt: 5 }}>
+      <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          Tasker
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Welcome to Tasker, your personal task manager app.
+        </Typography>
 
-export default home
+        <List>
+          <ListItem>
+            <ListItemText primary="Create, edit, and delete tasks" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Organize tasks by priority and due date" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Track your progress and stay productive" />
+          </ListItem>
+        </List>
+
+        <Button variant="contained" color="primary" size="large" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Home;
